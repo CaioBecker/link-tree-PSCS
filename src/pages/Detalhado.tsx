f7c94cec9ce323b20bd845a58803afb187f7ef93
Caio Becker <beckercaio31@gmail.com>
2025-09-03 14:34:57 -0300

    Revert "feature: protocolos"
    This reverts commit d20649a9178326e61ace8a34f3f9931c142a3aec.

--- src/pages/Detalhado.tsx
+++ src/pages/Detalhado.tsx
@@ -21,8 +21,8 @@ export default function DetalheBeneficiario() {
   };
 
   return (
-    <Container maxWidth="xl">
-      <Box sx={{ mt: 2, mb: 4}}>
+    <Container maxWidth="lg">
+      <Box sx={{ mt: 2, mb: 4 }}>
         <Button
           variant="outlined"
           startIcon={<ArrowBackIcon />}
@@ -66,7 +66,6 @@ export default function DetalheBeneficiario() {
             </Grid>
           </Grid>
         </Paper>
-        <Protocols />
       </Box>
     </Container>
   );
